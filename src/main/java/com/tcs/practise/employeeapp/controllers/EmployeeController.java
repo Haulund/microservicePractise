416d8801088822548b1247da6f15e4200d850fce
Haulund <steffen.haulund@gmail.com>
2023-03-12 12:11:52 +0100

    exception handling one solution

--- src/main/java/com/tcs/practise/employeeapp/controllers/EmployeeController.java
+++ src/main/java/com/tcs/practise/employeeapp/controllers/EmployeeController.java
@@ -3,9 +3,11 @@ package com.tcs.practise.employeeapp.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpInputMessage;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -41,12 +43,24 @@ public class EmployeeController {
     }
 
     @PutMapping("/api/employee")
-    public ResponseEntity<?> updateEmployee(@RequestBody Employee emp){
-        Employee updated = employeeService.update(emp);
-        if (updated == null) {
+    public ResponseEntity<?> updateEmployee(@RequestBody Employee emp) {
+         try {
+            Employee updated = employeeService.update(emp);
+            if (updated == null) {
+                return new ResponseEntity<>("wrong employee ID", HttpStatus.BAD_REQUEST);
+            }
+            return new ResponseEntity<>(updated, HttpStatus.OK);
+        } catch (TransactionSystemException e) {
+            throw new TransactionSystemException("Fields cannot be null"); 
+        } catch (HttpMessageNotReadableException e) {
+            throw new HttpMessageNotReadableException("Request needs a body", e.getHttpInputMessage());
+        }
+
+        //Employee updated = employeeService.update(emp);
+       /* if (updated == null) {
             return new ResponseEntity<>("wrong employee ID", HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>(updated, HttpStatus.OK);
+        return new ResponseEntity<>(updated, HttpStatus.OK); */
     }
 
     @DeleteMapping("/api/employee/{employeeId}")
