e590b1ffc364224f99df1082657dc8193f27fa3b
Haulund <steffen.haulund@gmail.com>
2023-03-16 14:04:44 +0100

    i18n implemtation

--- src/main/java/com/tcs/practise/employeeapp/controllers/HelloWorldController.java
+++ src/main/java/com/tcs/practise/employeeapp/controllers/HelloWorldController.java
@@ -1,7 +1,12 @@
 package com.tcs.practise.employeeapp.controllers;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private ResourceBundleMessageSource messageSource;
 
     @GetMapping("/hello-world")
     public String hellloWorld() {
@@ -27,4 +35,8 @@ public class HelloWorldController {
         return "Hello " + name;
     }
     
+    @GetMapping("hello/int")
+    public String i18nString(@RequestHeader(name = "Accept-language", required = false) String locale) {
+        return messageSource.getMessage("label.hello", null, new Locale(locale));
+    }
 }
